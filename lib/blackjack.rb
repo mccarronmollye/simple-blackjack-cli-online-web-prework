dea9cc4933377e5c12a6ba9e5e7b23c4e6c312fa
Molly McCarron <mccarronmollye@gmail.com>
2018-10-24 11:37:52 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,10 +35,15 @@ def hit?(card_total)
   if input == 's'
     return card_total
   elsif input == 'h'
-    return card_total + deal_card
+    return card_total += deal_card
+  else invalid_command
   end
 end
 
+
+def invalid_command
+  puts "Please enter a valid command"
+end
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
